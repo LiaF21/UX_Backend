4e81c4f8fd805f7155191f3d922eefca32a94170
LiaF21 <liaf21ramirez@gmail.com>
2024-05-08 21:17:09 -0600

    Rutas de Prueba

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,7 +3,7 @@ const userController = require('./userController');
 
 app.post('/auth/login', authController.login);
 router.get('/usuarios/:id', userController.getUserById);
-router.post('/', userController.createUser);
-router.delete('/', userController.deleteUserById);
-app.get('/', userController.getAllUsers);
+router.post('/usuarios/create', userController.createUser);
+router.delete('/usuarios/delete', userController.deleteUserById);
+app.get('/usuarios', userController.getAllUsers);
 
