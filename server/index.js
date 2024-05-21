b75c13c10fe732336e5ad2796bd200a51d51f5b2
nel <Recarte11nelson@hotmail.com>
2024-05-20 18:01:35 -0600

    add sequelize and make db connection

--- server/index.js
+++ server/index.js
@@ -1,19 +1,38 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const pool = require("./Db");
-const morgan = require("morgan")
+const cors = require('cors');
+const port = 3001;
+const morgan = require('morgan');
+const db = require('./Db')
+
+const sequelize = require('./Db');
 
 //Routes
 const routes = require('./routes/routes');
 
-
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 app.use(cors());
-app.use(express.json()); 
-app.use(routes)
+app.use(express.json());
+app.use(routes);
 //Esto puede ir en una ruta, servicio y controlador
 
-app.listen(3001, () => {
-  console.log("server has started on port 3001");
-});
+const initApp = async () => {
+  console.log('Testing the database connection..');
+  try {
+    await db.authenticate();
+    console.log('Connection has been established successfully.');
+
+    app.get('/', async (req, res) => {
+      //console.log(req);
+      return res.send('hello world');
+    });
+
+    app.listen(port, () => {
+      console.log(`Server is running at: http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+initApp();
