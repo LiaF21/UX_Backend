633ed950252f9bfd456c2326f55202210dad876b
Fabs <118944437+MissingLure@users.noreply.github.com>
2024-05-23 11:58:55 -0600

    Update Db.js

--- server/Db.js
+++ server/Db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('CasaDavid', 'postgres', 'MissingLure2005', {
-  host: 'localhost',
+const sequelize = new Sequelize('postgres', 'postgres', 'postgres', {
+  host: '54.91.54.29',
   dialect: 'postgres',
   logging: console.log,
 });
