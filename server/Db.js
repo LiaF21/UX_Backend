7bbd99fc9282909f1a0caba2bdea1d61f66c54d2
Fabs <fabriziojramos20@gmail.com>
2024-05-23 23:05:03 -0600

    Testing gitgnore

--- server/Db.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('postgres', 'postgres', 'postgres', {
-  host: '54.91.54.29',
-  dialect: 'postgres',
-  logging: console.log,
-});
-
-module.exports = sequelize;
