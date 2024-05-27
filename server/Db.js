dd3a100c8ab729ac324fd618bb7c427c9c54128e
elote200 <kelvin.melgar13@unitec.edu>
2024-05-27 09:34:12 -0600

    Host de aws
    La base de datos ya esta lista y poblada

--- server/Db.js
+++ server/Db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('CasaDavid', 'postgres', 'InsertarContraseñaAquiDeLaBDLocal', {
-  host: 'localhost',
+const sequelize = new Sequelize('CasaDavid', 'postgres', 'elote200', {
+  host: '44.202.106.102',
   dialect: 'postgres',
   logging: console.log,
 });
