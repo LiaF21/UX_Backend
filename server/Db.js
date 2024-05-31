8929a79b6e541890836e344cd1822fd53d6528d9
elote200 <kelvin.melgar13@unitec.edu>
2024-05-31 11:34:13 -0600

    Host de aws

--- server/Db.js
+++ server/Db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('CasaDavid', 'postgres', 'valeria', {
-  host: 'localhost',
+const sequelize = new Sequelize('CasaDavid', 'postgres', 'elote200', {
+  host: '184.73.20.14',
   dialect: 'postgres',
   logging: console.log,
 });
