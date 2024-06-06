a17b9c3768438da61a7ec3d0bcd8279f3af60c14
elote200 <kelvin.melgar13@unitec.edu>
2024-06-06 11:40:10 -0600

    Mas

--- server/controllers/huespedController.js
+++ server/controllers/huespedController.js
@@ -1,66 +1,73 @@
-const huespedService = require ('../services/huespedService');
-
+const huespedService = require("../services/huespedService");
 
 exports.getAllHuespedes = async (req, res) => {
-    try{
+  try {
     const huespedes = await huespedService.getAllHuespedes();
     res.status(201).json(huespedes);
-    } catch (error){
-      res.status(500).json({ error: error.message });
-    }
-};
-
-exports.getHuespedById = async (req, res) => {
-
-  try{
-    const huespedID = req.params.id;
-    const huesped = await huespedService.getHuespedById(huespedID);
-    if(huesped){
-      res.status(201).json(huesped);
-    }else{
-      res.status(404).json({message:'Huesped no encontrado,'});
-    }
-  }catch(error){
-    res.status(500).json({error:error.message});
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
 
-exports.createHuesped= async (req, res) => {
+exports.getAllHuespedesName = async (req, res) => {
   try {
-    const { id_huesped, id_persona, parentesco_paciente,reingreso} = req.body;
+    const huespedes = await huespedService.getAllHuespedesName();
+    res.status(201).json(huespedes);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.getHuespedById = async (req, res) => {
+  try {
+    const huespedID = req.params.id;
+    const huesped = await huespedService.getHuespedById(huespedID);
+    if (huesped) {
+      res.status(201).json(huesped);
+    } else {
+      res.status(404).json({ message: "Huesped no encontrado," });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.createHuesped = async (req, res) => {
+  try {
+    const { id_huesped, id_persona, parentesco_paciente, reingreso } = req.body;
     const nuevaHuesped = await huespedService.crearHuesped(req.body);
     res.status(201).json(nuevaHuesped);
   } catch (error) {
-    console.error('Error al crear huesped:', error);
-    res.status(500).json({ message: 'Error interno del servidor' });
+    console.error("Error al crear huesped:", error);
+    res.status(500).json({ message: "Error interno del servidor" });
   }
 };
 
 exports.deleteHuespedById = async (req, res) => {
   try {
-      const { id } = req.params;
-      const deleteHuesped = await huespedService.deleteHuespedById(id);
-      if (deleteHuesped) {
-          res.status(201).json({ message: 'Huesped eliminado exitosamente' });
-      } else {
-          res.status(404).json({ message: 'Huesped no encontrado' });
-      }
+    const { id } = req.params;
+    const deleteHuesped = await huespedService.deleteHuespedById(id);
+    if (deleteHuesped) {
+      res.status(201).json({ message: "Huesped eliminado exitosamente" });
+    } else {
+      res.status(404).json({ message: "Huesped no encontrado" });
+    }
   } catch (error) {
-      res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message });
   }
 };
 
-exports.editarHuesped= async(req, res)=>{
-  try{
-    const {id} = req.params;
+exports.editarHuesped = async (req, res) => {
+  try {
+    const { id } = req.params;
     const updated = req.body;
     const huespedEditado = await huespedService.editarHuesped(id, updated);
-    if(huespedEditado){
-      res.status(201).json({message: 'Huesped editado con exito'});
-    }else{
-      res.status(404).json({message: 'Error al editar Huesped.'});
+    if (huespedEditado) {
+      res.status(201).json({ message: "Huesped editado con exito" });
+    } else {
+      res.status(404).json({ message: "Error al editar Huesped." });
     }
-  }catch (error){
-    res.status(500).json({error: error.message});
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
--- server/services/huespedService.js
+++ server/services/huespedService.js
@@ -1,40 +1,67 @@
-const sequelize = require('../Db');
-const {Huesped} = require('../models/huesped');
+const sequelize = require("../Db");
+const { Huesped } = require("../models/huesped");
+const { Persona } = require("../models/persona");
+const { Reservacion, Cama, Habitacion } = require("../models/reservaciones");
 
-exports.getAllHuespedes = async () =>{
-    const huespedes = await Huesped.findAll({include: 'Persona'});
-    return huespedes;
+exports.getAllHuespedes = async () => {
+  const huespedes = await Huesped.findAll({ include: "Persona" });
+  return huespedes;
 };
 
-exports.getHuespedById = async (id) =>{
-    const huesped = await Huesped.findByPk(id);
-    return huesped;
+exports.getHuespedById = async (id) => {
+  const huesped = await Huesped.findByPk(id);
+  return huesped;
+};
+exports.getAllHuespedesName = async () => {
+  const huespedes = await Huesped.findAll({
+    include: [
+      {
+        model: Reservacion,
+        attributes: ["fecha_entrada", "fecha_salida"],
+        include: [
+          {
+            model: Cama,
+            attributes: ["nomre"],
+            include: [
+              {
+                model: Habitacion,
+                attributes: ["nombre"],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        model: Persona,
+        attributes: ["primer_nombre", "primer_apellido"],
+      },
+    ],
+  });
+  return huespedes;
+};
+exports.crearHuesped = async (huespedData) => {
+  const nuevoHuesped = await Huesped.create(huespedData);
+  return nuevoHuesped;
 };
 
-exports.crearHuesped = async (huespedData) =>{
-    const nuevoHuesped = await Huesped.create(huespedData);
-    return nuevoHuesped;
+exports.deleteHuespedById = async (id) => {
+  const borrar = await Huesped.destroy({
+    where: {
+      id_huesped: id,
+    },
+  });
+  return borrar;
 };
 
-exports.deleteHuespedById = async (id) =>{
-    const borrar = await Huesped.destroy({
-        where:{
-            id_huesped:id
-        }
+exports.editarHuesped = async (id, huespedUpdate) => {
+  const huespedEditado = await Huesped.update(huespedUpdate, {
+    where: { id_huesped: id },
+  });
+
+  if (huespedEditado) {
+    const edited = await Huesped.findOne({
+      where: { id_huesped: id },
     });
-    return borrar;
+    return edited;
+  }
 };
-
-exports.editarHuesped = async (id, huespedUpdate) =>{
-    const huespedEditado = await Huesped.update(huespedUpdate, {
-        where:{id_huesped:id}
-    });
-
-    if(huespedEditado){
-        const edited = await Huesped.findOne({ 
-            where: {id_huesped:id}
-        });
-        return edited;
-    }
-};
-
