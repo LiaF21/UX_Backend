6102458fd4ce612dac43791a1fa3b6e4d9e968ba
Fabs <fabriziojramos20@gmail.com>
2024-06-13 17:52:29 -0600

    Adjusted Service

--- server/services/listaSolicitudService.js
+++ server/services/listaSolicitudService.js
@@ -1,75 +1,130 @@
-const {ListaSolicitud} = require('../models/lista')
-const {Persona} = require('../models/persona')
-const {Huesped, PacienteHuesped} = require('../models/huesped')
+const { ListaSolicitud } = require("../models/lista");
+const { Persona, Ocupacion, Procedencia, Lugar } = require("../models/persona");
+const { Huesped, PacienteHuesped } = require("../models/huesped");
+const { Hospital } = require("../models/hospital");
+const Paciente = require("../models/paciente");
 
+exports.crearListaSolicitud = async (req) => {
+  const { id_lista_solicitud, id_persona, observacion, fecha_entrada } =
+    req.body;
+  const nuevaEspera = await ListaSolicitud.create({
+    id_lista_solicitud,
+    id_persona,
+    observacion,
+    fecha_entrada,
+  });
 
-exports.crearListaSolicitud = async (req) =>{
-   const {id_lista_solicitud,id_persona,observacion,fecha_entrada} = req.body;
-   const nuevaEspera = await ListaSolicitud.create({
-       id_lista_solicitud,
-       id_persona,
-       observacion,
-       fecha_entrada
-   })
-
-   return nuevaEspera
-}
+  return nuevaEspera;
+};
 exports.getSolicitudes = async () => {
-   try {
-     const solicitudes = await ListaSolicitud.findAll({
-       include: {
-         model: PacienteHuesped,
-         attributes: ['id_paciente_huesped'],
-         include: {
-           model: Huesped,
-           attributes: ['id_huesped'],
-           include: {
-             model: Persona,
-             attributes: ['genero', 'primer_nombre', 'primer_apellido', 'dni']
-           }
-         }
-       }
-     });
-     return solicitudes;
-   } catch (error) {
-     console.error("Error fetching solicitudes:", error);
-     throw error;
-   }
- };
- 
- 
- 
-exports.getAllListaSolicitud = async()=>{
-   const esperas = await ListaSolicitud.findAll();
-   return esperas
-}
+  try {
+    const solicitudes = await ListaSolicitud.findAll({
+      include: {
+        model: PacienteHuesped,
+        attributes: ["id_paciente_huesped"],
+        include: [{
+          model: Huesped,
+          include: 
+            {
+              model: Persona,
+              include: [
+                { model: Ocupacion },
+                { model: Procedencia },
+                { model: Lugar },
+              ]
+        }
+          },
+            {
+              model: Paciente,
+              include: [
+                {
+                  model: Persona,
+                  include: [
+                    { model: Ocupacion },
+                    { model: Procedencia },
+                    { model: Lugar },
+                  ],
+                },
+                {
+                  model: Hospital,
+                },
+              ],
+            },
+          ],
+        },
+      },
+    );
+    return solicitudes;
+  } catch (error) {
+    console.error("Error fetching solicitudes:", error);
+    throw error;
+  }
+};
 
-exports.getSolicitud = async(req,res)=>{
-   const {id} = req.params
-   const espera = await ListaSolicitud.findOne({
-      where:{
-         id_lista_solicitud:id
+exports.getAllListaSolicitud = async () => {
+  const esperas = await ListaSolicitud.findAll();
+  return esperas;
+};
+
+exports.getSolicitud = async (req, res) => {
+  const { id } = req.params;
+ console.log(id);
+  const espera = await ListaSolicitud.findOne({
+    where: {id_lista_solicitud:id},
+    include: {
+      model: PacienteHuesped,
+      attributes: ["id_paciente_huesped"],
+      include: [{
+        model: Huesped,
+        include: 
+          {
+            model: Persona,
+            include: [
+              { model: Ocupacion },
+              { model: Procedencia },
+              { model: Lugar },
+            ]
       }
-   })
-   return espera
-}
+        },
+          {
+            model: Paciente,
+            include: [
+              {
+                model: Persona,
+                include: [
+                  { model: Ocupacion },
+                  { model: Procedencia },
+                  { model: Lugar },
+                ],
+              },
+              {
+                model: Hospital,
+              },
+            ],
+          },
+        ],
+      },
+    },
+  );
 
-exports.editarListaSolicitud = async(req)=>{
-   const {id} = req.params;
-   const {id_persona,observacion} = req.body
-   const unaEspera = await ListaSolicitud.findByPk(id)
-   unaEspera.id_persona= id_persona;
-   unaEspera.observacion=observacion;
-   await unaEspera.save()
-   return unaEspera  
- }
+  return espera;
+};
 
- exports.eliminarSolicitud = async(req,res)=>{
-   const {id} = req.params;
-    await ListaSolicitud.destroy({
-      where:{
-         id_lista_solicitud:id,
-      }
-   })
-   
- }
+exports.editarListaSolicitud = async (req) => {
+  const { id } = req.params;
+  const { id_persona, observacion } = req.body;
+  const unaEspera = await ListaSolicitud.findByPk(id);
+  unaEspera.id_persona = id_persona;
+  unaEspera.observacion = observacion;
+  await unaEspera.save();
+  return unaEspera;
+};
+
+exports.eliminarSolicitud = async (req, res) => {
+  const { id } = req.params;
+  await ListaSolicitud.destroy({
+    where: {
+      id_lista_solicitud: id,
+    },
+  });
+};
