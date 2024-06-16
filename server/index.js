e9612613eedd8142fe40f5cf485783b5bc1e27a4
nel <Recarte11nelson@hotmail.com>
2024-06-16 15:17:24 -0600

    Token generation @login and verification in routes

--- server/index.js
+++ server/index.js
@@ -1,51 +1,41 @@
-require ('dotenv').config();
+require('dotenv').config();
 
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
-
-const morgan = require("morgan");
-const db = require("./Db");
+const cors = require('cors');
+const morgan = require('morgan');
+const db = require('./Db');
+const port = process.env.PORT || 3001;
+const authenticateJWT = require('./middleware/authenticateJWT');
 
 //Routes
-const routes = require("./routes/routes");
-const miscelanous = require("./routes/MiscelanousRoutes");
-const V = require("./routes/routesV");
-const excelRoutes = require("./routes/excelRoutes"); // nueva ruta
+const routes = require('./routes/routes');
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
+
+app.use(authenticateJWT);
 app.use(routes);
-
-app.use(miscelanous);
-app.use(V);
-app.use('/api', excelRoutes); // nueva ruta
-
-// const jwt = require("jsonwebtoken");
-
-const port = process.env.PORT || 3001;
-
-
 
 //Esto puede ir en una ruta, servicio y controlador
 
 const initApp = async () => {
-  console.log("Testing the database connection..");
+  console.log('Testing the database connection..');
   try {
     await db.authenticate();
-    console.log("Connection has been established successfully.");
+    console.log('Connection has been established successfully.');
 
-    app.get("/", async (req, res) => {
+    app.get('/', async (req, res) => {
       //console.log(req);
-      return res.send("hello world");
+      return res.send('hello world');
     });
 
     app.listen(port, () => {
       console.log(`Server is running at: http://localhost:${port}`);
     });
   } catch (error) {
-    console.error("Unable to connect to the database:", error.message);
+    console.error('Unable to connect to the database:', error.message);
     console.error('Parent error:', error.parent);
     console.error('Error details:', error);
   }
@@ -53,13 +43,12 @@ const initApp = async () => {
 
 db.sync({ force: false, alter: false})
   .then(() => {
-    console.log("Database synced without altering existing schema!");
+    console.log('Database synced without altering existing schema!');
   })
   .catch((error) => {
-    console.error("Error syncing database:", error.message);
+    console.error('Error syncing database:', error.message);
     console.error('Parent error:', error.parent);
     console.error('Error details:', error);
   });
-
 
 initApp();
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,28 +1,31 @@
-const {Router} = require('express')
+const { Router } = require('express');
 
 const rutasV = require('./routesV');
 const rutasF = require('./routesF');
 const rutasL = require('./productRoutes');
 const rutasA = require('./roomRoutes');
 const rutasD = require('./routesD');
-
+const rutasM = require('./MiscelanousRoutes');
 
 const router = Router();
 
 //rutas D = Ocupacion, Procedencia, Iglesia, Iglesia Huesped, Lista de Espera
-router.use( rutasD);
+router.use(rutasD);
 
 //rutasV = Users, Auth, Persona, Paciente, Paciente Huesped, Huesped, Lista Negra
-router.use( rutasV);
+router.use(rutasV);
 
 //rutas F = Privilegios, Usuario_Privilegio, Patrono, Afiliado, Afiliado Huesped, Patrono Huesped
-router.use( rutasF);
+router.use(rutasF);
 
 //rutasA = Habitacion, Cama, Reservacion
 router.use(rutasA);
 
 // rutasL = Transaccion, Reglamento, Hospital, Piso, Sala
-router.use( rutasL);
+router.use(rutasL);
+
+//rutasM = Miscelanous Routes
+router.use(rutasM);
 
 module.exports = router;
 //Depende de cuantas request vamos a necesitar, es más para tener ese orden por si por ejemplo
